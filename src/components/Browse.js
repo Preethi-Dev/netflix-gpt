1203624fd429c9aaa4a4baf21b60dd9590951a6b
preethi <selvampreethi360@gmail.com>
2024-02-29 13:53:48 +0530

    add Header to Browse page

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "./Header";
+import Signout from "./Signout";
 
 const Browse = () => {
-  return <div>Browse</div>;
+  return (
+    <div className="flex justify-between items-center">
+      <Header />
+      <Signout />
+    </div>
+  );
 };
 
 export default Browse;
