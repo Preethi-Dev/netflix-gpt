a2f19003ad6f9808374b44d2c1f40a706eeb5a2d
preethi <selvampreethi360@gmail.com>
2024-02-29 13:52:48 +0530

    add auth state & dispatch action on all auth states

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Login from "./Login";
 import Browse from "./Browse";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
 
 const Body = () => {
+  //dispatch
+  const dispatch = useDispatch();
   //create routing configuration using createBrowserRouter
   const appRouter = createBrowserRouter([
     {
@@ -15,6 +21,18 @@ const Body = () => {
       element: <Browse />,
     },
   ]);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName } = user;
+        dispatch(addUser({ uid, email, displayName }));
+      } else {
+        dispatch(removeUser());
+      }
+    });
+  }, []);
+
   return (
     <div>
       <RouterProvider router={appRouter} />
