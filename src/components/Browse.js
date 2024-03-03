1b010544abecc6d0fd648eca6fbed86ab56f6572
preethi <selvampreethi360@gmail.com>
2024-03-03 19:10:02 +0530

    call custom hooks in Browse component

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -2,13 +2,20 @@ import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import MainComponent from "./MainComponent";
 import SecondaryComponent from "./SecondaryComponent";
+import usePopularMovies from "../hooks/usePopularMovies";
+import useTopRatedMovies from "../hooks/useTopRatedMovies";
+import useUpcomingMovies from "../hooks/useUpcomingMovies";
 
 const Browse = () => {
   //side effects
   useNowPlayingMovies();
+  usePopularMovies();
+  useTopRatedMovies();
+  useUpcomingMovies();
 
   return (
     <div>
+      <div className="absolute w-full h-full bg-gradient-to-r from-black top-0 left-0 z-10"></div>
       <Header />
       <MainComponent />
       <SecondaryComponent />
