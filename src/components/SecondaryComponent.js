ddaf0488bd469156ec60adae00992af5703d560c
preethi <selvampreethi360@gmail.com>
2024-03-03 19:11:43 +0530

    add MovieList components with dedicated data by subscribe to the store

--- src/components/SecondaryComponent.js
+++ src/components/SecondaryComponent.js
@@ -1,7 +1,27 @@
 import React from "react";
+import MovieList from "./MovieList";
+import { useSelector } from "react-redux";
 
 const SecondaryComponent = () => {
-  return <div className="text-white">SecondaryComponent</div>;
+  const movies = useSelector((store) => store.movies);
+  return (
+    movies.nowPlayingMovies &&
+    movies.popularMovies &&
+    movies.popularMovies &&
+    movies.upcomingMovies && (
+      <div className="bg-black">
+        <div className="-mt-32 text-white pl-10 relative z-10">
+          <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies} />
+          <MovieList title={"Top Rated"} movies={movies?.topRatedMovies} />
+          <MovieList title={"Popular"} movies={movies?.popularMovies} />
+          <MovieList
+            title={"Upcoming Movies"}
+            movies={movies?.upcomingMovies}
+          />
+        </div>
+      </div>
+    )
+  );
 };
 
 export default SecondaryComponent;
