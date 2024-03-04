65df77a7d09240e075465d2864f7e34a396dc042
preethi <selvampreethi360@gmail.com>
2024-03-04 14:21:45 +0530

    support multilingual in gpt Search page

---
+++ src/utils/languageConstants.js
@@ -0,0 +1,16 @@
+const lang = {
+  en: {
+    search: "Search",
+    gptSearchPlaceholder: "what movie mood are you in?",
+  },
+  spanish: {
+    search: "Buscar",
+    gptSearchPlaceholder: "¿En qué estado de ánimo de película estás?",
+  },
+  hindi: {
+    search: "खोज",
+    gptSearchPlaceholder: "आप किस फिल्म के मूड में हैं?",
+  },
+};
+
+export default lang;
